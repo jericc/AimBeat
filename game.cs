18f8258ccf8174c1b83848a1c7a8be9c802e82ec
James-hello-world <102275594+James-hello-world@users.noreply.github.com>
2024-04-14 08:23:17 -0700

    Create game.cs

---
+++ game.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Examples : Monobehavior
+{
+    // start is called 
+    void Start()
+    {
+
+    }
+
+    //update is called
+    void Update()
+    {
+
+    }
+
+}
